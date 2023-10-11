7e65250c342871fb3beb5c5062084a44dec80d1b
Renann Souza <143777162+Renann2309@users.noreply.github.com>
2023-10-11 05:33:08 -0300

    Update PesoAeronave.java

--- PesoAeronave.java
+++ PesoAeronave.java
@@ -2,10 +2,31 @@
 public class PesoAeronave {
 import java.util.Scanner;
 
-	public static void main(String[] args) {
-	Scanner entrada = new Scanner(System.in);
+public static void main(String[] args) {
+   Scanner entrada = new Scanner(System.in);
+	
+	System.out.println("Peso máximo da aeronave: ");
+		int pesoMaximo = entrada.nextInt();
+	
+	System.out.println("Quantidade de passageiros: ");
+		int totalPassageiros = entrada.nextInt();
+	
+		int pesoTotalPassageiros = 0;
+	
+	for (int passageiroAtual = 1; passageiroAtual <= totalPassageiros; passageiroAtual++) {
+		System.out.printf("Peso do passageiro #%d: ", passageiroAtual);
+			int pesoPassageiro = entrada.nextInt();
+		
+		pesoTotalPassageiros += pesoPassageiro;
+	}
+	System.out.printf("Peso máximo da aeronave: %d kg%n", pesoMaximo);
+	System.out.printf("Peso total dos passageiros: %d kg%n", pesoTotalPassageiros);
+	System.out.printf("Situação da Aeronave: %s%n", pesoTotalPassageiros > pesoMaximo ? "peso excedido" : "Liberada");
 	
 
-	}
+
+    }
+	
+  }
 
 }
